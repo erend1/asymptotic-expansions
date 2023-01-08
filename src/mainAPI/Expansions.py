3d12cb96202e8aae0f62229ad41a2cbcd4cc4468
Hüseyin Eren <103572834+erend1@users.noreply.github.com>
2023-01-08 13:43:41 +0300

    get_all_results() function has been added.

--- src/mainAPI/Expansions.py
+++ src/mainAPI/Expansions.py
@@ -5,48 +5,30 @@ import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-
-
-class Sequence:
-
-    @staticmethod
-    def exponential(x, n):
-        """
-
-        :param float x:
-        :param int n:
-        :return:
-        """
-        result = ((-1)**n) * (math.exp(-x)) * (math.factorial(n)/x**(n+1))
-        return result
+from src.mainAPI.Sequences import Sequence
 
 
 class Expansion:
 
-    def __init__(self, sequence, x, n=None, sequence_start_index=0):
-        """
-
-        :param sequence:
-        :param float x:
-        :param int n:
-        :param int sequence_start_index:
-        """
-
-        self.sequence = sequence
+    def __init__(
+            self, sequence: Sequence, x: float, n: int = None
+    ):
+        self.seq = sequence
         self.x = float(x)
-        self.int_x = int(x)
         self.n = n
-        self.sequence_start_index = int(sequence_start_index)
+        self.seq_n0 = int(self.seq.n0)
         self.eval_flag = False
         self.results = list()
         self.error_results = list()
-        self._check_n()
-        self.eval()
+        self._backend_run()
 
-    def _check_n(self):
-        if self.n is None:
-            self.n = self.int_x
-            self._error_analysis()
+    def _backend_run(self):
+        if self.n is not None:
+            self.n = int(self.n)
+            self._error_analysis(analysis=False)
+        else:
+            self.n = int(self.x)
+            self._error_analysis(analysis=True)
         return self
 
     def _sum(self, temp_n=None):
@@ -54,17 +36,17 @@ class Expansion:
             temp_n = self.n
         total_sum = 0
         self.results = list()
-        for index in range(self.sequence_start_index, temp_n+1):
-            total_sum += self.sequence(x=self.x, n=index)
+        for index in range(self.seq_n0, temp_n + 1):
+            total_sum += self.seq.eval(x=self.x, n=index)
             self.results.append(total_sum)
         self.results.pop()
         return total_sum
 
     def _error_analysis(self, analysis=True):
-        prev_term = abs(self.sequence(x=self.x, n=self.sequence_start_index))
+        prev_term = abs(self.seq.eval(x=self.x, n=self.seq_n0))
         prev_error = math.inf
 
-        for index in range(self.sequence_start_index+1, self.n+1):
+        for index in range(self.seq_n0 + 1, self.n + 1):
             next_term = self._sum(temp_n=index)
             next_error = abs(next_term - prev_term)
             self.error_results.append(next_error)
@@ -77,14 +59,33 @@ class Expansion:
         self.eval_flag = True
         return self
 
-    def eval(self):
-        if self.eval_flag:
-            return self.results[-1]
-        else:
-            return self._sum()
+    def get_value(self):
+        return self.results[-1]
+
+    def get_all_results(self):
+
+        data_frame = pd.DataFrame(
+            {
+                "iteration": range(self.seq_n0, len(self.results)),
+                "result of expansion": self.results,
+                "error between each iteration": self.error_results
+            }
+        )
+
+        all_results = {
+            "data_frame": data_frame,
+            "x": self.x,
+            "n": self.n,
+            "n0": self.seq_n0,
+            "results": self.results,
+            "errors": self.error_results,
+            "iterations": range(self.seq_n0, len(self.results))
+        }
+
+        return all_results
 
     def graph(self, file_name="temp.pdf", path=""):
-        n_range = range(self.sequence_start_index, len(self.results))
+        n_range = range(self.seq_n0, len(self.results))
         plt.plot(n_range, self.results, 'bx-')
         plt.xlabel('n')
         plt.ylabel('Value of Asymptotic Expansion')
@@ -96,7 +97,7 @@ class Expansion:
     def error_graph(self, file_name="error.pdf", path=""):
         if len(self.error_results) == 0:
             self._error_analysis(analysis=False)
-        n_range = range(self.sequence_start_index, len(self.error_results))
+        n_range = range(self.seq_n0, len(self.error_results))
         plt.plot(n_range, self.error_results, 'bx-')
         plt.xlabel('n')
         plt.ylabel('Term Differences')
@@ -107,56 +108,10 @@ class Expansion:
 
 
 if __name__ == "__main__":
-
-    def main():
-        import matplotlib
-        from matplotlib import pyplot as plt
-        from matplotlib.backends.backend_pdf import PdfPages
-
-        #plt.rcParams["figure.figsize"] = [7.00, 3.50]
-        plt.rcParams["figure.autolayout"] = True
-
-        X = [1000]
-
-        seq = Sequence.exponential
-
-        for item in X:
-            temp_expns = Expansion(
-                sequence=seq, x=item
-            )
-
-            plt.figure()
-            temp_result = temp_expns.results
-            plt.plot(
-                range(0, len(temp_result)),
-                temp_result
-            )
-            plt.xlabel('n')
-            plt.ylabel('Term Differences')
-            plt.title(f"x = {item}")
-            print(f"--------------{item}-----------")
-            print(temp_expns.n)
-            print(temp_expns.eval())
-            try:
-                print((temp_expns.results[-1] - temp_expns.results[-2]) / (temp_expns.results[-3] - temp_expns.results[-4]))
-            except ZeroDivisionError:
-                pass
-            # PdfPages is a wrapper around pdf
-            # file so there is no clash and create
-            # files with no error.
-            p = PdfPages("test.pdf")
-
-            # get_fignums Return list of existing
-            # figure numbers
-            fig_nums = plt.get_fignums()
-            figs = [plt.figure(n) for n in fig_nums]
-
-            # iterating over the numbers in list
-            for fig in figs:
-                # and saving the files
-                fig.savefig(p, format='pdf')
-
-            # close the object
-            p.close()
-
-    main()
+    seq = Sequence()
+    expan = Expansion(
+        sequence=seq, x=10
+    )
+    print(
+        expan.results
+    )

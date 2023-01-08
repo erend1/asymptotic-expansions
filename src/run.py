82a0646475945828491c9ff038d13b472b3e3c42
Hüseyin Eren <103572834+erend1@users.noreply.github.com>
2023-01-08 13:44:55 +0300

    template_folder and static_folder path's specified.

--- src/run.py
+++ src/run.py
@@ -1,9 +1,13 @@
 from flask import Flask
 from src.utils.Utils import get_logger
-from src.restAPI.resorces import series, sequences
+from src.restAPI.resorces import series, sequences, crypto
 
 
-app = Flask(__name__)
+app = Flask(
+    __name__,
+    template_folder="restAPI/build/templates",
+    static_folder="restAPI/build/static"
+)
 
 logger = get_logger()
 
@@ -11,8 +15,13 @@ api_version = "/api/v1"
 
 app.register_blueprint(series.series, url_prefix=api_version)
 app.register_blueprint(sequences.sequences, url_prefix=api_version)
+app.register_blueprint(crypto.crypto, url_prefix=api_version)
 
 
 def run():
     logger.info("Rest server is started...")
     app.run()
+
+
+if __name__ == "__main__":
+    run()

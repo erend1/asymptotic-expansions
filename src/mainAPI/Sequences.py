7ce90e6b4a8eeae4d90361b3cc00c8fc15d5be57
Hüseyin Eren <103572834+erend1@users.noreply.github.com>
2023-01-11 21:38:42 +0300

    logarithmic_integral() sequence added.

--- src/mainAPI/Sequences.py
+++ src/mainAPI/Sequences.py
@@ -5,7 +5,8 @@ class Sequence:
 
     def __init__(self, sequence_name: str = None):
         self.seq_dict = {
-            "exponential": self._exponential
+            "exponential": self._exponential,
+            "logarithmic": self._logarithmic_integral
         }
         if sequence_name in self.seq_dict:
             self.main_seq = self.seq_dict[sequence_name]
@@ -43,4 +44,15 @@ class Sequence:
             r"\qquad x \to \infty .\]",
         ]
         result = ((-1) ** self.n) * (math.exp(-self.x)) * (math.factorial(self.n) / self.x ** (self.n + 1))
+        return result
+
+    def _logarithmic_integral(self):
+        self.n0 = 0
+        self.formulas_mat_jax = [
+            r"\text{li}",
+            r"\[ \text{li}( x ) = \int \limits _ 0 ^{ x } \dfrac{ \text{d}t }{ \ln(t)} , \qquad x >0. \]",
+            r"\[ \text{li}( x ) \sim \sum_{ k = 0 }^{ n } \dfrac{ x  k !}{ \ln ( x )^{ k + 1}}, "
+            r"\qquad x \to \infty .\]",
+        ]
+        result = ((self.x * math.factorial(self.n)) / (math.log(self.x) ** (self.n + 1)))
         return result

a67b371cf9498cc9f83c59240b889363828a7187
Hüseyin Eren <eren.demirtas@metu.edu.tr>
2023-03-13 12:54:13 +0300

    MongoDB Atlas account update.

--- src/utils/config.py
+++ src/utils/config.py
@@ -1,5 +1,19 @@
-# MongoDB Connection Configuration
-name = "metu"  # Name of the database to use.
-port = 27017  # Port of the connection.
-host = "localhost"  # Host of the connection.
-alias = "default"  # Alias of the connection.
+# --------------MongoDB Connection Configuration----------
+
+# Name of the database to use.
+name = "METU"
+
+# Port of the connection.
+port = 27017
+
+#  Host url for the connection.
+host_url = "mongodb+srv://onlineReader:<password>@metu.foimezd.mongodb.net/?retryWrites=true&w=majority"
+
+# Alias of the connection.
+alias = "default"
+
+# Password for the account.
+password = "qQptnG6nRLpM30yu"
+
+# Connection host.
+host = host_url.replace("<password>", password)

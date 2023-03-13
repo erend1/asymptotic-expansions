1844f3bdb8f2017ed36ab3df6509bb0804b1089f
Hüseyin Eren <eren.demirtas@metu.edu.tr>
2023-03-13 12:53:57 +0300

    Syntax fix.

--- src/restAPI/build/static/js/activate_button_radio.js
+++
@@ -1,9 +0,0 @@
-function ActivateButtonFromRadio(radio_id,button_id) {
-  const radio = document.getElementById(radio_id);
-  const btn = document.getElementById(button_id);
-  if (radio.checked === true) {
-    btn.setAttribute("aria-disabled", "true");
-  } else {
-    btn.setAttribute("aria-disabled", "true");
-  }
-}
